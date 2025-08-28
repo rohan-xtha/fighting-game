a6791704daf82670e606847bcad1cd7122679e15
rohan-xtha <rohan15december2005shrestha@gmail.com>
2025-08-28 07:48:14 +0545

    Update start.sh

--- fighting-game-web/start.sh
+++ fighting-game-web/start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+set -e  # Exit on error
+
+# Log environment
+env | sort
+
 # Install dependencies
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Check Python version
+echo "Python version:"
+python --version
+
 # Start the server
+echo "Starting server..."
 exec python server.py
